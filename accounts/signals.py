072c4be19ab0f1f03fa9b2b1d082a19d9f07f91d
brocode <kelimbth@gmail.com>
2024-03-02 15:06:00 +0300

    created signal to generate userID

---
+++ accounts/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import User
+import uuid
+
+
+@receiver(pre_save, sender=User)
+def generate_userID(sender, instance, **kwargs):
+    if instance.id == '':
+        instance.id = str(uuid.uuid4().hex)[:25]
