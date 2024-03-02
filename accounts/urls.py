c0c7dde881207106794e00f28de74f5b58b6bd34
brocode <kelimbth@gmail.com>
2024-03-02 14:57:24 +0300

    updated url paths for login and logout, and deleted url path for homepage

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns =[
-    path('', views.LogIn, name='login'),
-    path('home', views.Home, name='home'),
-    path('logout', views.Logout, name='logout'),
+    path('login/', views.UsersLoginView.as_view(), name='login'),
     path('user_profile', views.profileView, name='user_profile'),
+    path('logout/', views.LogoutUsersView.as_view, name='logout'),
+
 ]
