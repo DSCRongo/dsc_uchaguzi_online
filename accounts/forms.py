1ba958e00ef4eb319ada45b5606bd3f9f807c02c
brocode <kelimbth@gmail.com>
2024-03-02 15:26:42 +0300

    added file validation in ProfileForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from .models import User
+from .utils import is_image_file
 
 
 class ProfileForm(forms.ModelForm):
+    profile_pic = forms.FileField(
+        widget=forms.FileInput(attrs={
+            'type': 'file', 'class': 'form-control mb-2', 'accept': '.jpg, .jpeg, .png',
+        }),
+        required=False,
+        validators=[is_image_file],
+    )
+
     class Meta:
         model = User
         fields = ['email', 'gender', 'mobile_no', 'dob', 'age', 'profile_pic']
