d25d404d0107e576385fe1f3e7709cf94871a0f9
brocode <kelimbth@gmail.com>
2024-03-04 13:52:57 +0300

    created signal to generate voterID

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import User
+from .models import User, Voter
 import uuid
 
 
@@ -8,3 +8,10 @@ import uuid
 def generate_userID(sender, instance, **kwargs):
     if instance.id == '':
         instance.id = str(uuid.uuid4().hex)[:25]
+
+
+@receiver(pre_save, sender=Voter)
+def generate_voterID(sender, instance, **kwargs):
+    if instance.id == '':
+        instance.id = str(uuid.uuid4().hex)[:25]
+
