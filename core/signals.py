74c17fe6dee7fa37640eba42eef197fe0ce4d93e
brocode <kelimbth@gmail.com>
2024-03-04 10:45:54 +0300

    created signals to generate IDs

---
+++ core/signals.py
@@ -0,0 +1,23 @@
+from .models import Voter, Aspirant, VotingRecord
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+import uuid
+
+
+@receiver(pre_save, sender=Voter)
+def generate_voterID(sender, instance, **kwargs):
+    if instance.id == '':
+        instance.id = str(uuid.uuid4().hex)[:25]
+
+
+@receiver(pre_save, sender=Aspirant)
+def generate_aspirantID(sender, instance, **kwargs):
+    if instance.id == '':
+        instance.id = str(uuid.uuid4().hex)[:25]
+
+
+@receiver(pre_save, sender=VotingRecord)
+def generate_voter_recordID(sender, instance, **kwargs):
+    if instance.id == '':
+        instance.id = str(uuid.uuid4().hex)[:25]
+
