c10d906469ee6659b411df1e1be34d4ac1b4617d
brocode <kelimbth@gmail.com>
2024-03-04 11:52:24 +0300

    added brackets to call function 'as_view()'

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns =[
     path('login/', views.UsersLoginView.as_view(), name='login'),
     path('user_profile', views.profileView, name='user_profile'),
-    path('logout/', views.LogoutUsersView.as_view, name='logout'),
+    path('logout/', views.LogoutUsersView.as_view(), name='logout'),
 
 ]
