92bd1d353aeec762e8e6c3909376aba1f4ddf38f
brocode <kelimbth@gmail.com>
2024-03-04 12:07:41 +0300

    updated input fields, rearranged items and deleted age in list 'fields'

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,19 @@ from .utils import is_image_file
 
 
 class ProfileForm(forms.ModelForm):
+    SELECT_GENDER = (
+        (None, '-- Select gender --'),
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    email = forms.EmailField(widget=forms.EmailInput(), disabled=True)
+    dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), disabled=True)
+    gender = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select',
+        }),
+        choices=SELECT_GENDER,
+        disabled=True,
+    )
     profile_pic = forms.FileField(
         widget=forms.FileInput(attrs={
             'type': 'file', 'class': 'form-control mb-2', 'accept': '.jpg, .jpeg, .png',
@@ -14,4 +27,4 @@ class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['email', 'gender', 'mobile_no', 'dob', 'age', 'profile_pic']
+        fields = ['email', 'gender', 'dob', 'mobile_no', 'profile_pic']
