15e083c82c31e3cb521b82027413af2f5d9b0a22
brocode <kelimbth@gmail.com>
2024-03-04 10:49:13 +0300

    created HomepageView

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
 
-# Create your views here.
+
+@method_decorator(login_required(login_url='login'), name='get')
+class HomepageView(View):
+    template_name = 'core/homepage.html'
+
+
+    def get(self, request, *args, **kwargs):
+
+        context = {}
+        return redirect(request, self.template_name, context)
+    
+
