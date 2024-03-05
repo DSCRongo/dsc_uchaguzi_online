bcebcfca66ca09f6cea0883267ef53dc9e1986bc
brocode <kelimbth@gmail.com>
2024-03-05 16:30:20 +0300

    deleted url path for VoterRegistrationView

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns =[
     path('login/', views.UsersLoginView.as_view(), name='login'),
-    path('voter/registration/', views.VoterRegistrationView.as_view(), name='voter_registration'),
     path('user_profile', views.profileView, name='user_profile'),
     path('logout/', views.LogoutUsersView.as_view(), name='logout'),
 
