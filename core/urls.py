8daef3d80c1444fc571271c29a830aa0a08dfc74
brocode <kelimbth@gmail.com>
2024-03-05 14:54:49 +0300

    created url path for VotingView

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('homepage/', views.HomepageView.as_view(), name='homepage'),
+    path('<str:voter_id>/vote/', views.VotingView.as_view(), name='cast_vote'),
     
 ]
