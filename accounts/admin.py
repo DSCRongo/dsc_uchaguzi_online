96c7b42cd8aba4dcde2250112f2f3277a94c77cd
brocode <kelimbth@gmail.com>
2024-03-05 12:13:56 +0300

    updated user's form layout in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,21 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from .models import User
+from .forms import ProfileForm
 
 
-@admin.register(User)
-class UserTable(admin.ModelAdmin):
-    list_display = ('email', 'gender', 'mobile_no', 'dob', 'age', 'profile_pic', 'date_updated')
+class AdminPanelUserDetailsLayout(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'gender', 'dob', 'age', 'profile_pic', 'date_joined']
+    readonly_fields = ['last_login', 'date_joined']
+    
+
+    fieldsets = (
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'username', 'email', 'gender', 'dob', 'age', 'mobile_no',  'profile_pic',)
+        }),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(User, AdminPanelUserDetailsLayout)
