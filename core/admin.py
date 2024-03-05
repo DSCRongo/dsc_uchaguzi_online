5ea7a8ab08acd15a78e740d2054fcafe23f7710b
brocode <kelimbth@gmail.com>
2024-03-05 12:02:19 +0300

    registered models in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Aspirant, VotingRecord
 
-# Register your models here.
+
+@admin.register(Aspirant)
+class AspirantsTable(admin.ModelAdmin):
+    list_display = ['name', 'post', 'total_votes']
+
+
+@admin.register(VotingRecord)
+class CastedVotesTable(admin.ModelAdmin):
+    list_display = ['voter', 'elected_post']
