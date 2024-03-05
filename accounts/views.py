44d574c3ca0df690cc9d9a684b7fe2d078da9dd4
brocode <kelimbth@gmail.com>
2024-03-05 16:25:24 +0300

    deleted VoterRegistrationView I used a partial template 'register.html' to display voter registration form in homepage

--- accounts/views.py
+++ accounts/views.py
@@ -11,35 +11,6 @@ class UsersLoginView(LoginView):
     template_name = 'accounts/login.html'
 
 
-@method_decorator(login_required(login_url='login'), name='get')
-class VoterRegistrationView(View):
-    form_class = VoterRegistrationForm
-    template_name = 'accounts/register.html'
-
-    
-    def get(self, request, *args, **kwargs):
-        form = VoterRegistrationForm()
-
-        context = {'VoterRegistrationForm': form}
-        return render(request, self.template_name, context)
-    
-
-    def post(self, request, *args, **kwargs):
-        form = VoterRegistrationForm(request.POST)
-
-        if form.is_valid():
-            register_voter = form.save(commit=False)
-            register_voter.voters_name = request.user
-            register_voter.is_registered = True
-            register_voter.save()
-
-            messages.success(request, 'Voter details submitted successfully!')
-            return redirect('homepage')
-
-        context = {'VoterRegistrationForm': form}
-        return render(request, self.template_name, context)
-
-
 @login_required(login_url='login')
 def profileView(request):
     profile_form = ProfileForm(instance=request.user)
