ce5b6dc48b7542416f29e374e0958dd1fd297ae1
brocode <kelimbth@gmail.com>
2024-03-06 14:50:01 +0300

    updated form validator

--- accounts/validators.py
+++ accounts/validators.py
@@ -6,11 +6,12 @@ from accounts.models import Voter
 def check_registration_number_exists(request):
     """ This function validates if the typed registration number exists. """
     
-    registration_num = request.POST.get('reg-no')
+    registration_num = request.POST.get('reg_no')
+    print(f'Registration number: {registration_num}')
     reg_num_exists = Voter.objects.filter(reg_no__iexact=registration_num).exists()
 
     if reg_num_exists:
-        return HttpResponse('<div class="text-danger">Registration no. exists!</div>')
+        return HttpResponse('<div class="text-danger fw-bold">Registration number provided exists!</div>')
     
     else:
         return HttpResponse()
