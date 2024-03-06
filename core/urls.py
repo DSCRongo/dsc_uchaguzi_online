29aeedad92b7b73800d4155dba21ceae4db46d4e
brocode <kelimbth@gmail.com>
2024-03-06 12:16:54 +0300

    created url path for form validation

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from accounts import validators
 
 
 urlpatterns = [
@@ -7,3 +8,9 @@ urlpatterns = [
     path('<str:voter_id>/vote/', views.VotingView.as_view(), name='cast_vote'),
     
 ]
+
+htmx_urlpatterns = [
+    path('validate/registration-number/', validators.check_registration_number_exists, name='validate_registration_num'),
+]
+
+urlpatterns += htmx_urlpatterns
