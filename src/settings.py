2acca913ed1a657d7b58b91acb0400c0cb541e98
brocode <kelimbth@gmail.com>
2024-03-12 15:47:24 +0300

    updated MEDIA_URL

--- src/settings.py
+++ src/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR, 'static']
 
-MEDIA_URL = '/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
