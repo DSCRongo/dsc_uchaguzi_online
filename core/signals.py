db317c39a51644a2f6984efa9c284d2e4ba5dc46
brocode <kelimbth@gmail.com>
2024-03-12 15:37:05 +0300

    created signal to generated election ID

--- core/signals.py
+++ core/signals.py
@@ -1,4 +1,4 @@
-from .models import Aspirant, VotingRecord
+from .models import Aspirant, VotingRecord, ElectionsDate
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 import uuid
@@ -15,3 +15,8 @@ def generate_voter_recordID(sender, instance, **kwargs):
     if instance.id == '':
         instance.id = str(uuid.uuid4().hex)[:25]
 
+
+@receiver(pre_save, sender=ElectionsDate)
+def generate_voter_recordID(sender, instance, **kwargs):
+    if instance.id == '':
+        instance.id = str(uuid.uuid4().hex)[:25]
