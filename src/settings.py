22c9091085666e7defa01a485d04037aa8a35c79
brocode <kelimbth@gmail.com>
2024-03-13 15:15:25 +0300

    added AWS configurations

--- src/settings.py
+++ src/settings.py
@@ -134,6 +134,8 @@ else:
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -156,3 +158,10 @@ EMAIL_USE_SSL = False
 
 LOGIN_REDIRECT_URL = 'homepage'
 LOGOUT_REDIRECT_URL = 'login'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
