b6799328c6229944f8b376e6065fbdea57aaa54c
brocode <kelimbth@gmail.com>
2024-03-14 11:21:56 +0300

    overrode ready()

--- survey/apps.py
+++ survey/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SurveyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'survey'
+
+
+    def ready(self):
+        import survey.signals
