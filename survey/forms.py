9013b532f98883997e44264a77afb67e8ace4dde
brocode <kelimbth@gmail.com>
2024-03-14 12:01:05 +0300

    updated FeedbackForm - created new input field for rating and made other minor changes

--- survey/forms.py
+++ survey/forms.py
@@ -13,14 +13,22 @@ class FeedbackForm(forms.ModelForm):
     choices = forms.ChoiceField(widget=forms.CheckboxInput(attrs={
             'type': 'checkbox', 'class': 'mb-2',
         }),
+        label='How was the election conducted?',
+    )
+    rating = forms.CharField(widget=forms.NumberInput(attrs={
+            'type': 'number', 'class': 'mb-2',
+        }),
+        label='How do you rate the electoral process?',
+        help_text='Range is <b>1</b> to <b>5</b>'
     )
     description = forms.CharField(widget=forms.Textarea(attrs={
-            'class': 'mb-2', 'placeholder': 'Provide additional details about your feedback/complaint',
+            'class': 'mb-1', 
+            'placeholder': 'Provide additional details about your feedback/complaint about GDSC Uchaguzi platform ...',
         }),
         label='Description (optional)',
         required=False,
     )
-    
+
     class Meta:
         model = Feedback
-        fields = ('choices', 'description')
+        fields = ('choices', 'rating', 'description')
