40988acaf94c5281d1d485e2bf989bc320dd6e03
brocode <kelimbth@gmail.com>
2024-03-14 12:13:04 +0300

    appended survey url paths

--- src/urls.py
+++ src/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('core.urls')),
+    path('survey/', include('survey.urls')),
     re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 
 ]
