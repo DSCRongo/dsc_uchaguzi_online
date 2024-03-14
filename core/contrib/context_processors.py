f521439fbbd877a561ca3408df07e98f1795d5a3
brocode <kelimbth@gmail.com>
2024-03-14 20:53:12 +0300

    context processor

---
+++ core/contrib/context_processors.py
@@ -0,0 +1,10 @@
+from core.models import ElectionsDate
+from django.utils import timezone
+
+
+def elections(request):
+    current_dt = timezone.datetime.now().year
+    latest_election_date = ElectionsDate.objects.filter(election_date__year=current_dt).first()
+    context = {'election_is_over': latest_election_date.is_done}
+    return context
+
