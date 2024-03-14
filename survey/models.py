949ac958866db2d05f1308525c4ec21477b10b1a
brocode <kelimbth@gmail.com>
2024-03-14 11:38:25 +0300

    created feedback model

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Feedback(models.Model):
+    id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
+    choices = models.CharField(max_length=10, blank=False)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['-date_created']
+        verbose_name_plural = 'Feedback'
+
+
+    def __str__(self):
+        return self.id
+    
