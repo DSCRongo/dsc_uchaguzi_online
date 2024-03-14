20aa9922abcec8f174c0cba35ee9a15075a191ee
brocode <kelimbth@gmail.com>
2024-03-14 16:24:22 +0300

    added codeblock for countdown timer

--- static/js/main.js
+++ static/js/main.js
@@ -86,4 +86,21 @@
         document.body.addEventListener('htmx:afterRequest', hidePreloader);
 	}
 
+	// countdown timer
+	let countdown = select('.countdown');
+	const output = countdown.innerHTML;
+  
+	const countDownDate = function() {
+		let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
+	
+		let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+		let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) - 3;
+		let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+	
+		countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+	}
+	countDownDate();
+	setInterval(countDownDate, 1000);
+
 })();
