1b866701b27d6e2ac2bdac7249448e58591e7883
brocode <kelimbth@gmail.com>
2024-03-14 12:01:47 +0300

    created new field for rating

--- survey/models.py
+++ survey/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Feedback(models.Model):
     id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
     choices = models.CharField(max_length=10, blank=False)
+    rating = models.PositiveIntegerField(default=0)
     description = models.TextField(blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
